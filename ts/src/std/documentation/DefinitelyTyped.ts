fc496d90ad5d4e04dfc7b3d6fd51eee1f246ca90
Jeongho Nam <samchon@samchon.org>
2016-05-07 17:02:17 +0900

    v0.9.1

--- ts/src/std/documentation/DefinitelyTyped.ts
+++
@@ -1,4 +0,0 @@
-﻿// Type definitions for STL (Standard Template Library) Containers v0.8
-// Project: https://github.com/samchon/stl
-// Definitions by: Jeongho Nam <http://samchon.org>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
