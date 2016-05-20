90c82d44c34b65d2ebde5c6fc335419d32c40936
Jeongho Nam <samchon@samchon.org>
2016-05-20 17:11:44 +0900

    TypeScript-STL v0.9.7

--- commit.bat
+++ commit.bat
@@ -3,12 +3,13 @@ call build.bat
 :: ---------------------------------------------------------------------------------
 ::	Git Commit
 :: ---------------------------------------------------------------------------------
-git add .
-git commit -m "v0.9.7"
-git push origin master
+::git add .
+::git commit -m "v0.9.7"
+::git push origin master
 
 cd E:\Open_Source\DefinitelyTyped
-git add.
+git add .
 git commit -m "TypeScript-STL v0.9.7"
+git push origin master
 
 pause
