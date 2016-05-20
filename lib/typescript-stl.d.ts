6b8bc80e76fbdfa959eb5e1f323017a17fa7754b
Jeongho Nam <samchon@samchon.org>
2016-05-20 23:47:53 +0900

    v0.9.7

--- lib/typescript-stl.d.ts
+++ lib/typescript-stl.d.ts
@@ -1,3 +1,5 @@
+// Type definitions for TypeScript-STL v0.9.7
+// Project: https://github.com/samchon/stl
 // Definitions by: Jeongho Nam <http://samchon.org>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
