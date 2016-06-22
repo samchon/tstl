aa66827741577734f5edbbfc719e59e2b52f690b
Jeongho Nam <samchon@samchon.org>
2016-06-22 23:32:35 +0900

    v1.0

--- lib/typescript-stl.d.ts
+++ lib/typescript-stl.d.ts
@@ -9069,12 +9069,6 @@ declare namespace std.base {
     }
 }
 declare namespace std.base {
-    const MIN_SIZE: number;
-    const RATIO: number;
-    const MAX_RATIO: number;
-    function code(par: any): number;
-}
-declare namespace std.base {
     enum Hash {
         MIN_SIZE = 10,
         RATIO = 1,
