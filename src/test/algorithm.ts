3d405e4941e136b517c44b0638bb6eea3e259ee4
Jeongho Nam <samchon@samchon.org>
2017-03-13 00:01:22 +0900

    v1.4.3

--- src/test/algorithm.ts
+++ src/test/algorithm.ts
@@ -6,10 +6,12 @@ namespace example
 	{
 		let array: std.Vector<number> = new std.Vector<number>();
 		for (let i: number = 1; i <= 15; i++)
-			array.push_back(i);
-
-		console.log(array.data());
+			for (let j: number = 0; j < 3; j++)
+				array.push_back(i);
+		
 		std.shuffle(array.begin(), array.end());
+		std.stable_sort(array.begin(), array.end());
+		console.log(std.is_sorted(array.begin(), array.end()));
 		console.log(array.data());
 	}
 }
