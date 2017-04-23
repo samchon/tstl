2ae2459d309e3419a7bf2fd25db603f387ee3cbf
Jeongho Nam <samchon@samchon.org>
2017-04-23 14:11:55 +0900

    Vector's copy constructor

--- src/test/API.ts
+++ src/test/API.ts
@@ -1,3 +1,23 @@
 ﻿/// <reference path="../../lib/tstl.d.ts" />
 
 eval('var std = require("../../lib/tstl")');
+
+namespace example
+{
+	export function main(): void
+	{
+		console.log("TEST ALL");
+		
+		for (let key in example)
+			if (key != "main" && (example as any)[key] instanceof Function)
+			{
+				console.log("===================================================");
+				console.log("	" + key);
+				console.log("===================================================");
+
+				(example as any)[key]();
+				console.log("\n");
+			}
+	}
+}
+module.exports = example;
