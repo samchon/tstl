e3f1bdd3374c64ceb4f0c37d999975f62d9b935d
Jeongho Nam <samchon@samchon.org>
2017-04-29 22:52:58 +0900

    For_of to v1.4

--- src/std/typings/tstl/tstl.d.ts
+++ src/std/typings/tstl/tstl.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for TSTL v1.4.9
+// Type definitions for TSTL v1.4.10
 // Project: https://github.com/samchon/tstl
 // Definitions by: Jeongho Nam <http://samchon.org>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
