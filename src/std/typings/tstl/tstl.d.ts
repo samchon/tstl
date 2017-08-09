eb77d4f3dd5dd413d269e4a1d6196098ac7b50ca
Jeongho Nam <samchon@samchon.org>
2017-08-09 23:41:24 +0900

    exports

--- src/std/typings/tstl/tstl.d.ts
+++ src/std/typings/tstl/tstl.d.ts
@@ -3,8 +3,5 @@
 // Definitions by: Jeongho Nam <http://samchon.org>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module "tstl"
-{
-	export = std;
-}
+export = std;
 
