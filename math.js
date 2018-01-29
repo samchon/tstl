e579e4bb8340aa2b993f4c83d75a382446a96cf4
Samchon <sam@myrealplan.co.kr>
2018-01-29 16:17:31 +0900

    bessel functions, but wrong results

--- math.js
+++
@@ -1,6 +0,0 @@
-const std = require("./lib/tstl");
-
-console.log(std.cyl_bessel_i(0, 1.2345));
-console.log(std.cyl_bessel_j(0, 1.2345));
-console.log(std.cyl_bessel_k(0.5, 1.2345));
-console.log(std.sph_bessel(1, 1.2345));
