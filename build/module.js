45ea3822091fa595ca0f0172021a03e9c82147c6
Samchon <sam@myrealplan.co.kr>
2018-02-08 10:47:16 +0900

    build/module.js

--- build/module.js
+++ build/module.js
@@ -4,14 +4,46 @@ const Global = require("./global");
 
 function main()
 {
+	// COMPILE & EXPORT SYMBOL
 	compile();
 	attach_header();
 	
-	replace_body('["', '"]', (value) => 
+	// REMOVE EVALS
+	replace_body(content =>
 	{
-		return "." + value;
+		let first = content.indexOf('eval("');
+		let last = content.indexOf('");', first + 1);
+
+		if (first == -1 || last == -1)
+			return null;
+
+		let repl = content.substring(first + 6, last);
+		return content.substr(0, first) + repl + content.substr(last + 3);
 	});
-	replace_body('eval("', '");', (value) => {return value;});
+
+	// REMOVE DYNAMICS
+	replace_body(content =>
+	{
+		let first = -1;
+
+		while (true)
+		{
+			first = content.indexOf('["', first + 1);
+			let last = content.indexOf('"]', first + 1);
+
+			if (first == -1 || last == -1)
+				return null;
+			
+			let repl = "." + content.substring(first + 2, last);
+			if (repl.indexOf(",") != -1)
+			{
+				first = last;
+				continue;
+			}
+			else
+				return content.substr(0, first) + repl + content.substr(last + 2);
+		}
+	});
 }
 
 function compile()
@@ -36,32 +68,26 @@ function attach_header()
 	const HEAD = Global.SOURCE_PATH + "/typings/" + Global.FILE_NAME + ".d.ts";
 	const BODY = Global.RELEASE_PATH + "/" + Global.FILE_NAME + ".d.ts";
 
-	let text = fs.readFileSync(HEAD, "utf8");
-	text += fs.readFileSync(BODY, "utf8");
+	let content = fs.readFileSync(HEAD, "utf8");
+	content += fs.readFileSync(BODY, "utf8");
 
-	fs.writeFileSync(BODY, text, "utf8");
+	fs.writeFileSync(BODY, content, "utf8");
 }
 
-function replace_body(S1, S2, changer)
+function replace_body(replacer)
 {
 	const JS_FILE = Global.RELEASE_PATH + "/" + Global.FILE_NAME + ".js";
-	var text = fs.readFileSync(JS_FILE, "utf8");
-	if (text.indexOf(S1) == -1)
-		return;
-
-	let segments = text.split(S1);
-	for (let part of segments)
+	let content = fs.readFileSync(JS_FILE, "utf8");
+	
+	while (true)
 	{
-		if (part.indexOf(S2) == -1)
-			continue;
-
-		let value = part.substr(0, part.indexOf(S2));
-		let org = S1 + value + S2;
-		let repl = changer(value);
-
-		text = text.split(org).join(repl);
+		let repl = replacer(content);
+		if (repl == null)
+			break;
+		else
+			content = repl;
 	}
-	fs.writeFileSync(JS_FILE, text, "utf8");
+	fs.writeFileSync(JS_FILE, content, "utf8");
 }
 
 main();
