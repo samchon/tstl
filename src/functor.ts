62450b499f32900cbbbfe84e4acaeb7dca176af8
Jeongho Nam <samchon@samchon.org>
2018-03-11 14:48:12 +0900

    Ready

---
+++ src/functor.ts
@@ -0,0 +1,5 @@
+export * from "./functors/exception";
+export * from "./functors/functional";
+export * from "./functors/special_math";
+export * from "./functors/thread";
+export * from "./functors/utility";
