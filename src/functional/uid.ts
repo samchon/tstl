d59bb6000b86eddefdb2597a3570517b842a8cc6
Samchon <sam@myrealplan.co.kr>
2018-05-25 15:14:03 +0900

    v1.8.1 Publish

--- src/functional/uid.ts
+++ src/functional/uid.ts
@@ -5,18 +5,36 @@ import { is_node } from "../utilities/node";
  */
 var root_: IGlobal = null;
 
+/**
+ * @hidden
+ */
+function _Get_root(): IGlobal
+{
+	if (root_ === null)
+	{
+		root_ = (is_node() ? global : window) as IGlobal;
+		if (root_.__s_iUID === undefined)
+			root_.__s_iUID = 0;
+	}
+	return root_;
+}
+
+/**
+ * Get unique identifier.
+ * 
+ * @param obj Target object.
+ * @return The identifier number.
+ */
 export function get_uid(obj: Object): number
 {
 	// FOR THE OLDER VERSION (UNDER V1.8)
 	if (Object.prototype.hasOwnProperty("__get_m_iUID"))
 		return (obj as any).__get_m_iUID();
-	else
-		_Initialize();
 	
 	// NO UID EXISTS, THEN ISSUE ONE.
 	if (obj.hasOwnProperty("__m_iUID") === false)
 	{
-		var uid: number = ++root_.__s_iUID;
+		var uid: number = ++_Get_root().__s_iUID;
 		Object.defineProperty(obj, "__m_iUID", 
 		{
 			get: function (): number
@@ -28,19 +46,6 @@ export function get_uid(obj: Object): number
 
 	// RETURNS
 	return (obj as any).__m_iUID;
-}
-
-/**
- * @hidden
- */
-function _Initialize(): void
-{
-	if (root_ != null)
-		return;
-	
-	root_ = (is_node() ? global : window) as IGlobal;
-	if (root_.__s_iUID === undefined)
-		root_.__s_iUID = 0;
 }
 
 /**
